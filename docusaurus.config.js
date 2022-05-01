754a91c13f033b76af220631a05bc06dafe06992
Sanam <sanampakuwal27@gmail.com>
2022-05-01 19:25:40 +0545

    chore: code cleanup

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "BLC BIT Notes",
   tagline: "Bachelor of Information Technology Notes",
-  url: "/notes/",
+  url: "https://blc-bit.github.io",
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",

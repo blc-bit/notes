3de51f4f5e85574ec4f72194e6d44107563c4048
Sanam <sanampakuwal27@gmail.com>
2022-05-01 19:02:01 +0545

    chore: code cleanup

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -91,7 +91,7 @@ const config = {
               },
               {
                 label: "GitHub",
-                href: "https://github.com/facebook/docusaurus",
+                href: "https://github.com/blc-bit/notes",
               },
             ],
           },

8c3de67f3f17244749d6682c14a77f7008ab3de3
sanamhub <sanamhub@users.noreply.github.com>
2022-05-01 12:49:45 +0000

    deploy: 7b1a6c9ba1b2eabe1497d916428ca7b6c95d0c01

---
+++ assets/js/ce05bb0c.0c5d8fbe.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunknotes=self.webpackChunknotes||[]).push([[1683],{3774:function(e){e.exports=JSON.parse('{"allTagsPath":"/blog/tags","name":"greetings","count":1,"permalink":"/blog/tags/greetings"}')}}]);

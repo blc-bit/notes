480b2e150b6836d4eefd90309981dd95e18443e9
Sanam <sanampakuwal27@gmail.com>
2022-05-01 19:30:57 +0545

    chore: code cleanup

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: "BLC BIT Notes",
   tagline: "Bachelor of Information Technology Notes",
   url: "https://blc-bit.github.io",
-  baseUrl: "/",
+  baseUrl: "/notes/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
